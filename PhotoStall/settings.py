36282bd4a690bfbd2809683a8893e80da2bd4714
VDA-001 <vdavishu@gmail.com>
2021-07-09 11:10:14 +0530

    Changed staticfiles directory

--- PhotoStall/settings.py
+++ PhotoStall/settings.py
@@ -168,7 +168,7 @@ USE_TZ = True
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
