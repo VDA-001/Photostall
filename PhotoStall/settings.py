240e9c39132e9292b4cf3ed5101195adf13e97e1
Vishal D A <67089703+VDA-001@users.noreply.github.com>
2021-07-09 11:32:30 +0530

    Update settings.py

--- PhotoStall/settings.py
+++ PhotoStall/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.staticfiles.storage.ManifestStaticFilesStorage',
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
